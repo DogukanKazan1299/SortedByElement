0897d09d8fafd4b15872640e7da5bdea95d3a3d4
Doğukan KAZAN <dogukan.kazan@atolla.com.tr>
2022-07-20 12:26:23 +0300

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int[] array = new int[] { 4, 13, 8, 9, 7, 1, 6 };
+            int[] array = new int[] { 16, 23, 7, 11, 3, 14 };
+            newSort(array);
+
+        }
+
+        public static int[] newSort(int[] array)
+        {
+            Array.Sort(array);
+            Array.Reverse(array);
+
+            int[] newArray= new int[array.Length];
+
+            int k = 0;
+            int length = array.Length - 1;
+
+            for(int i=0; i<array.Length; i = i + 2, k++)
+            {
+                newArray[k] = array[i];
+            }
+
+            for(int j=1; j<array.Length; j = j + 2, length--)
+            {
+                if (newArray[length] == 0)
+                    newArray[length] = array[j];
+            }
+            return newArray;
+        }
+    }
+}
